6298e2b189061fac59cb94a28318e06458cc8fd3
KahoriDiaz <kahori.diaz@estudiante.ucsm.edu.pe>
2026-03-28 00:11:28 -0500

    EJERCICIO 3

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -5,22 +5,34 @@ public class CaesarShiftCipher {
     public String CaesarShiftCipher(String message, int shift){
         StringBuilder sb = new StringBuilder();
         char currentChar;
-        int length = message.length();
 
-        shift = shift%26;
+        shift = shift % 26;
 
-        for(int i = 0; i < length; i++){
+        for(int i = 0; i < message.length(); i++){
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
-                return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
-                currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
-                currentChar = (char) (currentChar + 26);
+
+            // permitir espacio
+            if (currentChar == ' ') {
+                sb.append(currentChar);
+                continue;
             }
-            sb.append((char) (currentChar + shift));
+
+            // validar letras
+            if (currentChar < 'a' || currentChar > 'z') {
+                return "invalid";
+            }
+
+            // aplicar desplazamiento
+            char shifted = (char) (currentChar + shift);
+
+            // ajustar límites
+            if (shifted > 'z') {
+                shifted -= 26;
+            } else if (shifted < 'a') {
+                shifted += 26;
+            }
+
+            sb.append(shifted);
         }
 
         return sb.toString();
